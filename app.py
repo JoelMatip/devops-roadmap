5a69d6e10cfac3edec94e74337f92ae4d6a810a4
Ajay K.S <aks599552@gmail.com>
2025-10-25 21:50:24 +0530

    Day 12: Added reset button and route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import redirect, url_for
 import redis
 import os
 
@@ -10,6 +11,11 @@ r = redis.Redis.from_url(redis_url)
 def index():
     count = r.incr("hits")
     return render_template("index.html", count=count)
+
+@app.route("/reset", methods=["POST"])
+def reset():
+    r.set("hits", 0)
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
