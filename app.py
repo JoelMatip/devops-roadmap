aa2a253da7bff836b93eec7f1a023103b489a754
Ajay K.S <aks599552@gmail.com>
2025-10-25 22:06:20 +0530

    Day 12: Added reset button and /reset route

--- app.py
+++ app.py
@@ -17,5 +17,9 @@ def reset():
     r.set("hits", 0)
     return redirect(url_for("index"))
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
