0f69d85166d618e96197f6415154b52ccb8d0f2e
Ajay K.S <aks599552@gmail.com>
2025-10-26 11:39:50 +0530

    Day 14: Passed variables into /about template

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def reset():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", name="Ajay", version="1.0", redis_status=r.ping())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
